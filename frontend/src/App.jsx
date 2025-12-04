00c559129c98b12fb40c3b07f63c92907531da4f
KishorePrabakar <kishorefun21@gmail.com>
2025-12-04 17:55:15 +0530

    Basic Frontend to build backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  return (<>
+    <h1>VSBEC HostelCare</h1>
+    <form>
+      <label>Enter your name:
+        <input type="text" />
+      </label>
+      <br />
+      <br />
+      <label>Room number:
+        <input type="number" />
+      </label>
+      <br />
+      <br />
+      <label>Complaint type:
+        <select name="complaint_type" id="ctype">
+          <option value="furniture">Furniture</option>
+          <option value="water">Water</option>
+          <option value="electricity">Electricity</option>
+          <option value="damage">Damage</option>
+          <option value="others">Others</option>
+        </select>
+      </label>
+      <br /><br />
+      <label>Description:
+        <textarea name="desc" id="desc"></textarea>
+      </label>
+      <br /><br />
+      <input type="submit" value="Submit" />
+    </form>
+  </>
   )
 }
 
